a8a98423be1dee88098b07a6bb92afbe9eb2a3f1
Vizienishka <viziehafiyyann@gmail.com>
2025-12-19 21:22:08 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -20,8 +20,7 @@
         <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
             
             <div class="bg-white p-6 rounded-[20px] border border-gray-100 shadow-sm flex items-center gap-4 hover:shadow-md transition cursor-default">
-                <div class="w-12 h-12 rounded-xl bg-blue-50 flex items-center justify-center text-blue-600">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <div class="w-12 h-12  flex items-center justify-center text-blue-600">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5z" />
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
                     </svg>
@@ -35,8 +34,7 @@
             </div>
 
             <div class="bg-white p-6 rounded-[20px] border border-gray-100 shadow-sm flex items-center gap-4 hover:shadow-md transition cursor-default">
-                <div class="w-12 h-12 rounded-xl bg-indigo-50 flex items-center justify-center text-indigo-600">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <div class="w-12 h-12  flex items-center justify-center text-indigo-600">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                     </svg>
                 </div>
@@ -49,8 +47,7 @@
             </div>
 
             <div class="bg-white p-6 rounded-[20px] border border-gray-100 shadow-sm flex items-center gap-4 hover:shadow-md transition cursor-default">
-                <div class="w-12 h-12 rounded-xl bg-orange-50 flex items-center justify-center text-orange-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <div class="w-12 h-12  flex items-center justify-center text-orange-500">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0z" />
                     </svg>
                 </div>
